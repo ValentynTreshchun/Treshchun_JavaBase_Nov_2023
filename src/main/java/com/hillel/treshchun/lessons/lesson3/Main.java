668432f3ef86f2d98ee73137db55ad9e4b29685b
Valentyn Treshchun <webberrbn@gmail.com>
2023-11-27 21:03:17 +0200

    sout hello git 2nd time

--- src/main/java/com/hillel/treshchun/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/treshchun/lessons/lesson3/Main.java
@@ -3,5 +3,8 @@ package com.hillel.treshchun.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
