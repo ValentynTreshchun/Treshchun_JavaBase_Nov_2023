8af3d9b5a60591660318de6768cde132e765177f
Valentyn Treshchun <webberrbn@gmail.com>
2023-11-30 01:18:52 +0200

    new main_2 class added

---
+++ src/main/java/com/hillel/treshchun/lessons/lesson3/Main_2.java
@@ -0,0 +1,8 @@
+package com.hillel.treshchun.lessons.lesson3;
+
+public class Main_2 {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis()*1000);
+//        Printing time in secondss
+    }
+}
