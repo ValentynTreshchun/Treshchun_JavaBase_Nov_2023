adc75546779ce475eed87bb7a52acb3b934a4b52
Valentyn Treshchun <webberrbn@gmail.com>
2023-11-29 23:34:13 +0200

    sout hello git 2nd time

--- src/main/java/com/hillel/treshchun/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/treshchun/lessons/lesson3/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+//      Repository name: Treshchun_JBasic_23_Nov2023
     }
 }
