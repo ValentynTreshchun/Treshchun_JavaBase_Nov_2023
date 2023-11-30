b374f7727059eb8fe7fb0b5ac1b312ab3df1aba5
ValentynTreshchun <152213455+ValentynTreshchun@users.noreply.github.com>
2023-11-30 02:09:25 +0200

    Add second println to Main_2 class

--- src/main/java/com/hillel/treshchun/lessons/lesson3/Main_2.java
+++ src/main/java/com/hillel/treshchun/lessons/lesson3/Main_2.java
@@ -3,7 +3,8 @@ package com.hillel.treshchun.lessons.lesson3;
 public class Main_2 {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis()*1000);
-//        Printing time in secondss
+//        Printing time in seconds
+        System.out.println("This line was added on GitHub");
 
     }
 }
