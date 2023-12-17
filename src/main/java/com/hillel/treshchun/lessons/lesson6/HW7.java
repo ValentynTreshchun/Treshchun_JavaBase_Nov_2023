9f7356126e31b3a22d939c2eee7b06ddf3c6bd51
Valentyn Treshchun <webberrbn@gmail.com>
2023-12-17 15:59:43 +0200

    homework 7 - some minot formatting adjustments made

--- src/main/java/com/hillel/treshchun/lessons/lesson6/HW7.java
+++ src/main/java/com/hillel/treshchun/lessons/lesson6/HW7.java
@@ -11,9 +11,9 @@ public class HW7 {
             int randNumber = (int) (Math.random() * 11);
             int guessNumber = 0;
             for (int i = 3; i > 0; i--) {
-                System.out.println("Enter your guess: ");
+                System.out.print("\nEnter your guess: ");
                 while (!scanner.hasNextInt()){
-                        System.out.println("Wrong data entered, please enter an integer");
+                        System.out.print("Wrong data entered, please enter an integer: ");
                         scanner = new Scanner(System.in);
 //                        starting a new stream instead of "scanner.nextLine();" as I could not find a better way of having multiple \n in input stream
 //                        which resulted in "while" cycle repeating more than once. is there a better way?
@@ -28,10 +28,10 @@ public class HW7 {
                 }
             }
             if (guessNumber != randNumber){
-                System.out.println("You lost. The true random number was " + randNumber + "\n");
+                System.out.println("You lost. The true random number was " + randNumber);
                 scanner.nextLine();
             }
-            System.out.println("Would you like to play again? Y/N: ");
+            System.out.print("Would you like to play again? Y/N: ");
             gameRepeat = scanner.nextLine();
         } while (gameRepeat.equals("y") || gameRepeat.equals("Y") || gameRepeat.equals("yes") || gameRepeat.equals("Yes"));
         System.out.println("Game over");
